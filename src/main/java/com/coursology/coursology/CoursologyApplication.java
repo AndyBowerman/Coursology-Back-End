cdf3b7f1bbb908774681d12c6421436e18f2f3e6
AndyBowerman <andy_bowerman06@hotmail.com>
2023-01-13 11:18:28 +0000

    project set up

---
+++ src/main/java/com/coursology/coursology/CoursologyApplication.java
@@ -0,0 +1,13 @@
+package com.coursology.coursology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursology/coursology/CoursologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursology.coursology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
